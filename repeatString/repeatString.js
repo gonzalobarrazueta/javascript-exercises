499043840453c54d36f6125897d4100bdf6e6d81
gonzalo.b <u20191a972@upc.edu.pe>
2020-09-13 20:09:31 -0500

    🥰  Solved first 3 exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(repeatedString, repetitionNumber) {
 
+
+    let s = (repetitionNumber < 0) ? 'ERROR' : '';
+
+    for( let  i = 0; i < repetitionNumber; i++){
+
+        s += repeatedString;
+    }
+
+    return s;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(s) {
 
+    let sReversed = '';
+
+    for(let i = s.length -1; i > -1; i--){
+
+        sReversed += s[i];
+    }
+    
+    return sReversed;
 }
 
 module.exports = reverseString
