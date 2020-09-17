842bf2d7393514178047b3c0368a15a312aa37fe
gonzalo.b <u20191a972@upc.edu.pe>
2020-09-17 14:55:49 -0500

    🥰  finished all exercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
 
+    let args = Array.prototype.slice.call(arguments);
+    args.splice(0,1); //le quito el 1er elemento que es el array con el cual debo comparar
+
+    let indexOfElement = 0;
+
+    for(let i = 0 ; i < args.length; i++){
+        
+        indexOfElement = arr.indexOf(args[i]);
+        
+        // chequeo si el elemento existe o no 
+        if (indexOfElement < 0 || typeof(args[i]) !== typeof(arr[indexOfElement])) continue;
+
+        arr.splice(indexOfElement, 1);
+    
+    }
+    return arr;
+
+    /* Example with rest parameter
+
+    let indexToEliminate;
+
+    for( let i = 0; i < moreArgs.length; i++){
+        
+        indexToEliminate = arr.indexOf(moreArgs[i]);
+        if(indexToEliminate === -1) continue;
+
+        arr.splice(indexToEliminate, 1);
+    }
+
+    return arr; */
 }
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if(
+    (num1 < 0 || num2 < 0) ||
+    (typeof(num1) !== 'number' || typeof(num2) !== 'number')
+    ){
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    let i;
+    let n;
+    
+    if (num1 > num2){
+        i = num2;
+        n = num1;
+    } else {
+
+        i = num1;
+        n = num2;
+    }
+
+    for(; i <= n; i++){
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
